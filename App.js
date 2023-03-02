f7ad0d8d605128b0d2c84b273650e1f2d96c5739
Ibrohimjon  Muhammadjonov <115447690+Ibrohimqor1@users.noreply.github.com>
2023-03-02 11:05:48 +0500

    Add files via upload

---
+++ App.js
@@ -0,0 +1,238 @@
+function App() {
+  return (
+    <div className="App">
+      {/* Padding //////////////////////////////////
+       <div class="pt-4">pt-6</div>
+      <div class="pl-4">pr-4</div>
+      <div class="pb-8">pb-8</div>
+      <div class="pl-2 ">pl-2</div> */}
+
+
+
+
+
+
+
+
+
+      
+      {/* <div class="px-8">px-8</div>
+
+      <div class="py-8">py-8</div> */}
+
+
+
+
+
+
+
+
+
+
+
+      {/*
+      Margin/////////////////////////////////////
+      
+      <div class="mt-6">mt-6</div>
+      <div class="mr-4 ">mr-4</div>
+      <div class="mb-8 ">mb-8</div>
+      <div class="ml-2 ">ml-2</div> */}
+
+
+
+
+
+
+
+
+
+      {/* <div class="mx-8 ...">mx-8</div>
+
+      <div class="my-8 ...">my-8</div> */}
+
+
+
+
+
+
+
+
+
+
+      {/* width ////////////////////////////////
+      <div class="w-96 bg-green-500">sssssssss</div>
+      <div class="w-80 "></div>
+      <div class="w-64"></div>
+      <div class="w-48 "></div>
+      <div class="w-40"></div>
+      <div class="w-32 "></div>
+      <div class="w-24"></div> */}
+
+
+
+
+
+
+
+
+
+      {/* height////////////////////////////////////////
+      <div class="h-96 ...">h-96</div>
+      <div class="h-80 ...">h-80</div>
+      <div class="h-64 ...">h-64</div>
+      <div class="h-48 ...">h-48</div>
+      <div class="h-40 ...">h-40</div>
+      <div class="h-32 ...">h-32</div>
+      <div class="h-24 ...">h-24</div> */}
+
+
+
+
+
+
+
+
+      {/* flex //////////////////////////////////////////////////
+      <div class="flex ...">
+        <div class="w-1/2 bg-green-500">w-1/2</div>
+        <div class="w-1/2 bg-red-700 ">w-1/2</div>
+      </div>
+      <div class="flex ...">
+        <div class="w-2/5 ...">w-2/5</div>
+        <div class="w-3/5 ...">w-3/5</div>
+      </div>
+      <div class="flex ...">
+        <div class="w-1/3 ...">w-1/3</div>
+        <div class="w-2/3 ...">w-2/3</div>
+      </div>
+      <div class="flex ...">
+        <div class="w-1/4 ...">w-1/4</div>
+        <div class="w-3/4 ...">w-3/4</div>
+      </div>
+      <div class="flex ...">
+        <div class="w-1/5 ...">w-1/5</div>
+        <div class="w-4/5 ...">w-4/5</div>
+      </div>
+      <div class="flex ...">
+        <div class="w-1/6 ...">w-1/6</div>
+        <div class="w-5/6 ...">w-5/6</div>
+      </div>
+      <div class="w-full ...">w-full</div>
+      ////////////////////////////////////////////////////////// */}
+
+
+
+
+
+
+
+
+
+
+      {/* Font Family /////////////////////////////////////////
+      <p class="font-sans ...">The quick brown fox ...</p>
+      <p class="font-serif ...">The quick brown fox ...</p>
+      <p class="font-mono ...">The quick brown fox ...</p>
+      /////////////////////////////////////////////*/}
+
+
+
+
+
+      {/* Font-sizing //////////////////////////////////////////
+      <p class="text-sm ...">The quick brown fox ...</p>
+      <p class="text-base ...">The quick brown fox ...</p>
+      <p class="text-lg ...">The quick brown fox ...</p>
+      <p class="text-xl ...">The quick brown fox ...</p>
+      <p class="text-2xl ...">The quick brown fox ...</p>
+      ///////////////////////////////////////////////// */}
+
+
+
+
+
+
+      {/* border ///////////////////////////////////////////
+      <div class="border border-indigo-600 ...">ssss</div>
+      <div class="border-2 border-indigo-600 ...">ssssss</div>
+      <div class="border-4 border-indigo-600 ...">sssss</div>
+      <div class="border-8 border-indigo-600 ...">ssss</div>
+      //////////////////////////////////////// */}
+
+
+
+
+
+      {/* shadow ///////////////////////////////////////////
+      <div class="shadow-md ...">sssssssssssss</div>
+      <div class="shadow-lg ...">ssssssssssss</div>
+      <div class="shadow-xl ...">sssssssssss</div>
+      <div class="shadow-2xl ...">sssssssssss</div>
+      ///////////////////////////////////////// */}
+
+
+
+
+
+      {/* brigyhness /////////////////////////////////////////////
+      <div class="brightness-50 ..."></div>
+      <div class="brightness-100 ..."></div>
+      <div class="brightness-125 ..."></div>
+      <div class="brightness-200 ..."></div>
+      ///////////////////////////////////////////////// */}
+
+
+
+
+
+
+      {/* Skeleton  //////////////////////////////////////////////////
+      <div class="border border-blue-300 shadow rounded-md p-4 max-w-sm w-full mx-auto">
+        <div class="animate-pulse flex space-x-4">
+          <div class="rounded-full bg-slate-200 h-10 w-10"></div>
+          <div class="flex-1 space-y-6 py-1">
+            <div class="h-2 bg-slate-200 rounded"></div>
+            <div class="space-y-3">
+              <div class="grid grid-cols-3 gap-4">
+                <div class="h-2 bg-slate-200 rounded col-span-2"></div>
+                <div class="h-2 bg-slate-200 rounded col-span-1"></div>
+              </div>
+              <div class="h-2 bg-slate-200 rounded"></div>
+            </div>
+          </div>
+        </div>
+      </div>
+      ////
+      /////////////////////////////////////////////////// */}
+
+
+
+
+
+      {/* Animation ////////////////////////////////////////////////
+      <span class="relative flex h-3 w-3">
+        <span class="animate-ping absolute inline-flex h-full w-full rounded-full bg-sky-400 opacity-75"></span>
+        <span class="relative inline-flex rounded-full h-3 w-3 bg-sky-500"></span>
+      </span>{" "}
+      ////////////////////////////////////////////////////////// */}
+
+
+
+
+      {/* flex/////////////////////////////////////////////
+      <div>
+        When controlling the flow of text, using the CSS property
+        <span class="inline">display: inline</span>
+        will cause the text inside the element to wrap normally. While using the
+        property <span class="inline-block">display: inline-block</span>
+        will wrap the element to prevent the text inside from extending beyond
+        its parent. Lastly, using the property{" "}
+        <span class="block">display: block</span>
+        will put the element on its own line and fill its parent.
+      </div>
+      //////////////////////////////////////////// */}
+    </div>
+  );
+}
+
+export default App;
